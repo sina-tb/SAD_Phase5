d6f9d1f5bc7c427b835fbd604de6808c3c9a4e4e
AliAtaollahi <aliataollai40@gmail.com>
2023-07-05 12:42:28 +0330

    Add files

---
+++ project/Back_end/DAO.py
@@ -0,0 +1,13 @@
+class DAO:
+    def __init__(self):
+        pass
+
+    def make_get_prerequisities_query(self, package_id):
+        query = ''
+        # TO DO
+        return query
+
+    def make_add_record_query(self, prerequisities, package_id, user_name):
+        query = ''
+        # TO DO
+        return query
