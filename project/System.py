3a35194faf4e23851603cb8fc7f1a69067070464
AliAtaollahi <aliataollai40@gmail.com>
2023-07-05 12:09:23 +0330

    Add fuctions

--- project/System.py
+++ project/System.py
@@ -14,23 +14,35 @@ class System:
         print(command._header)
         print(command._body)
 
-    def logout(self):
+    def logout(self, command):
+        self._status = manual.PageStatus.CLOSE_PROGRAM
+        self._interface.show_page(self._status)
         return
 
-    def request_new_package(self):
+    def request_new_package(self, command):
+        self._status = manual.PageStatus.REQUEST_NEW_PACKGE
+        self._interface.show_page(self._status)
+
+    def fill_prerequisites(self, command):
+        package_id = command._body['package_id']
+        self._status = manual.PageStatus.FILL_PREREQUISITES
+        self._interface.show_page(self._status)
+
+    def show_message(self, command):
+        prerequisities = command._body['prerequisities']
+        self._status = manual.PageStatus.SUCCESSFULL_ADD_REQUEST_MESSAGE
+        self._interface.show_page(self._status)
+
+    def return_to_home(self, command):
+        self._status = manual.PageStatus.HOME_PAGE
+        self._interface.show_page(self._status)
+
+    def return_to_select_package(self, command):
+        self._status = manual.PageStatus.REQUEST_NEW_PACKGE
+        self._interface.show_page(self._status)
+
+    def show_last_status_of_requests(self, command):
         pass
 
-    def select_package(self):
-        pass
-
-    def show_last_status_of_requests(self):
-        pass
-
-    def show_profile(self):
-        pass
-
-    def show_message(self):
-        pass
-
-    def fill_prerequisites(self):
+    def show_profile(self, command):
         pass
